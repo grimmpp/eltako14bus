e470213e526be9443e53bbcd2c560373b1f56dbd
chrysn <chrysn@fsfe.org>
2018-09-10 18:58:54 +0200

    Serial: Add echo suppression
    This is automatically enabled if echos are detected; the detection
    process causes some noise on the line.
    
    Echo happens with RS485 drivers that do not have their receive- and
    not-transmit-enable lines wired together, for example in the FAM14 when
    accessed as a USB device.

--- eltakobus/serial.py
+++ eltakobus/serial.py
@@ -1,4 +1,5 @@
 import asyncio
+import time
 
 import serial_asyncio
 
@@ -12,15 +13,40 @@ class RS485SerialInterface(BusInterface):
     Note that this relies on the UART to be configured to drive the bus only
     when data is being sent, as for example done by the Digitus adapters.
     """
-    def __init__(self, filename):
+    def __init__(self, filename, suppress_echo=None):
         self._filename = filename
 
         self.received = asyncio.Queue()
         self._hook = None
 
+        self.suppress_echo = suppress_echo
+        self._suppress = []
+
     async def run(self, loop, *, conn_made=None):
         self._loop = loop
         reader, self._writer = await serial_asyncio.open_serial_connection(url=self._filename, baudrate=57600, loop=loop)
+
+        if self.suppress_echo is None:
+            # any character sequence as long as it won't look like the preamble and confuse other bus participants
+            for i in range(5):
+                # flush the input; FIXME tapping into StreamReader internals means I should implement a protocol
+                reader._buffer.clear()
+                echotest = b'\xff\x00\xff'
+                self._writer.write(echotest)
+                try:
+                    read = await asyncio.wait_for(reader.readexactly(len(echotest)), timeout=0.1, loop=self._loop)
+                except asyncio.TimeoutError:
+                    self.suppress_echo = False
+                    break
+                if read == echotest:
+                    self.suppress_echo = True
+                    break
+                else:
+                    await asyncio.sleep(0.5)
+                    continue
+            else:
+                conn_made.set_exception(RuntimeError("Echo detection failed"))
+                return
 
         if conn_made is not None:
             conn_made.set_result(None)
@@ -34,6 +60,15 @@ class RS485SerialInterface(BusInterface):
                 # TBD: determine whether this shows up somewhere, and develop a signaling strategy
                 raise
             while len(buffered) >= 14:
+                if self.suppress_echo:
+                    # purge entries older than 10s, they might have had
+                    # collisions and thus did not report correctly
+                    while self._suppress and self._suppress[0][0] < time.time() - 10:
+                        self._suppress.pop(0)
+                    if self._suppress and buffered[:14] == self._suppress[0][1]:
+                        buffered = buffered[14:]
+                        continue
+
                 try:
                     parsed = ESP2Message.parse(buffered[:14])
                 except ParseError:
@@ -84,7 +119,7 @@ class RS485SerialInterface(BusInterface):
         try:
             self._hook = hook
 
-            self._writer.write(request.serialize())
+            await self.send(request)
             try:
                 # FIXME this timeout is rather arbitrary
                 return await asyncio.wait_for(match, timeout=1, loop=self._loop)
@@ -97,6 +132,9 @@ class RS485SerialInterface(BusInterface):
             self._hook = None
 
     async def send(self, request):
-        self._writer.write(request.serialize())
+        serialized = request.serialize()
+        if self.suppress_echo:
+            self._suppress.append((time.time(), serialized))
+        self._writer.write(serialized)
 
     base_exchange = None
