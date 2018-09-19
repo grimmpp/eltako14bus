4d684b2875451f6764314f158ab5eabfa7588e58
chrysn <chrysn@fsfe.org>
2018-09-19 15:09:04 +0200

    provide setup.py
    this might ease home assistant setups

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import setuptools
+
+setuptools.setup(
+    name="eltakobus",
+    version="0.0.1",
+    author="chrysn",
+    author_email="chrysn@fsfe.org",
+    description="Library for participating in the Eltako Series 14 RS485 bus",
+    url="https://gitlab.com/chrysn/eltakobus",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
