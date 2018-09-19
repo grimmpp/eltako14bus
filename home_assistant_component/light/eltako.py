73edb24370043604a6eb4d9821725ff0e41d826d
chrysn <chrysn@fsfe.org>
2018-09-19 14:39:17 +0200

    home assistant component moved into dedicated repo

--- home_assistant_component/light/eltako.py
+++
@@ -1,6 +0,0 @@
-DEPENDENCIES = ['eltako']
-
-from .. import eltako
-
-async def async_setup_platform(hass, config, add_entities, discovery_info=None):
-    eltako.platforms['light'].set_result(add_entities)
