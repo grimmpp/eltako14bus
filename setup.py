6de5cfeeac11f3b6f4b936ded9e8e6ac2ff3579e
chrysn <chrysn@fsfe.org>
2018-11-16 08:29:06 +0100

    add tests requires (without actual tests)

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 import setuptools
 
+extras_require = {
+    'serial': ['pyserial_asyncio', 'pyserial >= 3.4'],
+    'coap': ['aiocoap == 0.4a1'],
+    # Not usable yet as it's not exported via an entry point
+    'eltakotool': ['pyyaml', 'pyxdg'],
+}
+
 setuptools.setup(
     name="eltakobus",
     version="0.0.3",
@@ -9,12 +16,9 @@ setuptools.setup(
     description="Library for participating in the Eltako Series 14 RS485 bus",
     url="https://gitlab.com/chrysn/eltakobus",
     packages=setuptools.find_packages(),
-    extras_require={
-        'serial': ['pyserial_asyncio', 'pyserial >= 3.4'],
-        'coap': ['aiocoap == 0.4a1'],
-        # Not usable yet as it's not exported via an entry point
-        'eltakotool': ['pyyaml', 'pyxdg'],
-        },
+    extras_require=extras_require,
+    # Not that there'd be tests, but at least it fetches the right dependencies and syntax checks everything
+    tests_require=list(set(sum(extras_require.values(), []))),
     classifiers=[
         "Programming Language :: Python :: 3",
     ],
