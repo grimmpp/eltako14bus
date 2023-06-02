643af02d6f7e467d40f2d0988d871cb03001dcf5
Philipp Grimm <grimm.philipp@gmail.com>
2023-06-02 22:17:41 +0200

    changed lib name

--- setup.py
+++ setup.py
@@ -12,12 +12,12 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="eltakobus",
+    name="eltako14bus",
     version="0.0.27",
     author="chrysn, grimmpp",
     author_email="chrysn@fsfe.org, grimmpp14@gmail.com",
     description="Library for participating in the Eltako Series 14 RS485 bus",
-    url="https://gitlab.com/chrysn/eltakobus",
+    url="https://github.com/grimmpp/eltako14bus",
     packages=setuptools.find_packages(),
     extras_require=extras_require,
     # Not that there'd be tests, but at least it fetches the right dependencies and syntax checks everything
